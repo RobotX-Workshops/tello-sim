9762453f7ca6965b1fa3b201c76bd07875523245
Anujith Muraleedharan <140189296+AnujithM@users.noreply.github.com>
2025-03-15 01:03:01 +0530

    fix

--- example_exercises/14_speed.py
+++ example_exercises/14_speed.py
@@ -1,16 +1,20 @@
 from tello_sim.tello_sim_client import TelloSimClient
 
-
 import time
 
-
-TEST_DISTANCE = 30
+TEST_DISTANCE = 500
 
 # Create a Tello instance
 tello = TelloSimClient()
 
 # Connect to Tello
 tello.connect()
+
+def pause(next_action: str):
+    print(f"Hovering before {next_action}")
+    for i in range(3, 0, -1):
+        print(i)
+        time.sleep(1)
 
 print("Starting flying in ...")
 for i in range(3, 0, -1):
@@ -21,21 +25,18 @@ for i in range(3, 0, -1):
 print("Take off")
 tello.takeoff()
 
+pause("ascending")
 tello.move_up(100)
 
-
 for speed in range(10, 101, 10):
+    pause(f"speed change to {speed} cm/s")
     print("Setting speed to", speed, "cm/s")
     tello.set_speed(speed)
-    print("Testing forward and back at given speed")
-    tello.move_forward(30)
+    print(f"Testing forward and back at {speed} cm/s")
+    tello.move_forward(1000)
     tello.move_back(TEST_DISTANCE)
     time.sleep(1)
 
-
+pause("landing")
 print("Finished Landing")
-# Land
 tello.land()
-
-# End the connection
-tello.end()
